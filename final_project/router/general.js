0d5589f0af828bb8c59f2e68447a523c40a11322
Oscar Rieken <oriekenjr@gmail.com>
2025-08-20 22:36:20 -0500

    :sparkles: (Task 3) added /author/:author

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -29,8 +29,14 @@ public_users.get('/isbn/:isbn',function (req, res) {
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const author = req.params.author;
+  const currentBooks = Object.values(books);
+  const filteredBooks = currentBooks.filter(book => book.author === author);
+
+  if (!filteredBooks.length) {
+    return res.status(404).json({ message: "No books found for the given author"});
+  }
+  return res.send(JSON.stringify(filteredBooks));
 });
 
 // Get all books based on title
